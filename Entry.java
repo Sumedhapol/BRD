266ede4f3cfe0bf7c7d0672627522329d04e866c
smn5914-rgb <smn5914@psu.edu>
2026-04-16 15:56:42 -0400

    Implement Entry class with attributes and methods
    Added fields for sleep hours, stress level, study hours, and mood score with corresponding getters and setters.

--- Entry.java
+++ Entry.java
@@ -1,1 +1,58 @@
+public class Entry {
+    private double sleepHours;
+    private int stressLevel;
+    private double studyHours;
+    private int moodScore;
 
+    public Entry() {
+    }
+
+    public Entry(double sleepHours, int stressLevel, double studyHours, int moodScore) {
+        this.sleepHours = sleepHours;
+        this.stressLevel = stressLevel;
+        this.studyHours = studyHours;
+        this.moodScore = moodScore;
+    }
+
+    public double getSleepHours() {
+        return sleepHours;
+    }
+
+    public void setSleepHours(double sleepHours) {
+        this.sleepHours = sleepHours;
+    }
+
+    public int getStressLevel() {
+        return stressLevel;
+    }
+
+    public void setStressLevel(int stressLevel) {
+        this.stressLevel = stressLevel;
+    }
+
+    public double getStudyHours() {
+        return studyHours;
+    }
+
+    public void setStudyHours(double studyHours) {
+        this.studyHours = studyHours;
+    }
+
+    public int getMoodScore() {
+        return moodScore;
+    }
+
+    public void setMoodScore(int moodScore) {
+        this.moodScore = moodScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Entry{" +
+                "sleepHours=" + sleepHours +
+                ", stressLevel=" + stressLevel +
+                ", studyHours=" + studyHours +
+                ", moodScore=" + moodScore +
+                '}';
+    }
+}
