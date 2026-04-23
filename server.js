0ebfa615cd95c0c764f7af1b8f186a55622f21fe
Sumedhapol <smp6989@psu.edu>
2026-04-23 13:30:36 -0400

    Set up Express server with MongoDB connection

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/entries", require("./routes/entries"));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
