265620f9da669340791f1921cb4986934bf60634
smn5914-rgb <smn5914@psu.edu>
2026-04-23 13:31:14 -0400

    Create Entry model with schema for user data

--- models/Entry.js
+++ models/Entry.js
@@ -1,1 +1,42 @@
+const mongoose = require("mongoose");
 
+const entrySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    sleep: {
+      type: Number,
+      required: true
+    },
+    stress: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10
+    },
+    study: {
+      type: Number,
+      required: true
+    },
+    mood: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10
+    },
+    score: {
+      type: Number,
+      required: true
+    },
+    riskLevel: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Entry", entrySchema);
